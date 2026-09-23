d41556a5b7e110c59856dc0b458fefd213c3b05d
Edwin Jaya <91804364+Edwin-Jaya@users.noreply.github.com>
2026-09-23 17:03:38 +0700

    fix auth image

--- src/app/shared/pipe/auth-image-pipe.ts
+++ src/app/shared/pipe/auth-image-pipe.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
+import { authContext } from '../../core/interceptor/auth-interceptor';
 
 @Pipe({
   name: 'authImage',
@@ -14,11 +15,17 @@ export class AuthImagePipe implements PipeTransform {
   ) {}
 
   transform(url: string): Observable<SafeUrl> {
-    return this.http.get(url, { responseType: 'blob' }).pipe(
-      map((val: Blob) => {
-        const objectURL = URL.createObjectURL(val);
-        return this.sanitizer.bypassSecurityTrustUrl(objectURL);
-      }),
-    );
+    return this.http
+      .get(url, {
+        responseType: 'blob',
+        context: authContext(),
+        withCredentials: true,
+      })
+      .pipe(
+        map((val: Blob) => {
+          const objectURL = URL.createObjectURL(val);
+          return this.sanitizer.bypassSecurityTrustUrl(objectURL);
+        }),
+      );
   }
 }
